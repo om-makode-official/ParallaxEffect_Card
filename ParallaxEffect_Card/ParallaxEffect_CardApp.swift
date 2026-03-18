26359c0ceb473c1c9daf923219e5339c22d87791
Om Kashinath Makode <omkashinath_m@nexteducation.in>
2026-03-18 17:32:40 +0530

    Initial Commit

---
+++ ParallaxEffect_Card/ParallaxEffect_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxEffect_CardApp.swift
+//  ParallaxEffect_Card
+//
+//  Created by Sai Krishna on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxEffect_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
